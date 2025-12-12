37d4e86ecce40e2a35b1884f73e24c196129c847
여재호\duwog <smartway.jhyeo@gmail.com>
2025-12-12 11:51:58 +0900

    feat: 푸터 정보 스마트로드로 업데이트

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -15,25 +15,24 @@ export function Footer() {
       <div className="max-w-4xl mx-auto text-center space-y-4">
         {/* Company Name & Address */}
         <div className="space-y-1">
-          <h3 className="text-white font-bold text-lg">라인테크</h3>
-          <p className="text-white/60 text-sm">경기도 남양주시 별내중앙로26, 506호</p>
+          <h3 className="text-white font-bold text-lg">스마트로드</h3>
+          <p className="text-white/60 text-sm">인천광역시 남동구 은봉로297 305호</p>
         </div>
 
         {/* Contact Info */}
         <div className="space-y-1">
-          <p className="text-white/60 text-sm">전화: 031-575-1012</p>
-          <p className="text-white/60 text-sm">이메일: linetech2012@naver.com</p>
+          <p className="text-white/60 text-sm">전화: 010-4806-9911</p>
+          <p className="text-white/60 text-sm">이메일: smartroad.jhyeo@gmail.com</p>
         </div>
 
         {/* Registration Numbers */}
         <div className="space-y-1">
-          <p className="text-white/60 text-sm">사업자등록번호: 132-86-03287</p>
-          <p className="text-white/60 text-sm">법인등록번호: 284111-0101653</p>
+          <p className="text-white/60 text-sm">사업자등록번호: 605-44-77540</p>
         </div>
 
         {/* Copyright */}
         <div className="pt-4 border-t border-white/10">
-          <p className="text-white/40 text-xs">© 2024 라인테크. All rights reserved.</p>
+          <p className="text-white/40 text-xs">© 2024 스마트로드. All rights reserved.</p>
         </div>
       </div>
     </footer>
